2725d0781d02faa7d616daa1adc251c1e6458f08
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-18 13:23:31 +0000

    Snapshot of branch master taken on 2019-09-18T09:23:30.134-04:00 from "https://github.com/UnofficialJuliaMirror/Libcroco_jll.jl-57eb2189-7eb1-52c8-ac0e-99495f550b14"

---
+++ src/Libcroco_jll.jl
@@ -0,0 +1,31 @@
+module Libcroco_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("57eb2189-7eb1-52c8-ac0e-99495f550b14"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Libcroco", artifacts_toml) for e in artifacts["Libcroco"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Libcroco; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Libcroco_jll
